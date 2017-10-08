7d33e270f553eb485086571f7e798ca264f0b327
komatsu <tkgitttr8@gmail.com>
2017-10-08 04:54:23 +0000

    complete

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :reviews, only: [:index, :destroy]
+  root 'books#index'
+  resources :books do
+    resources :reviews, except: [:index, :destroy]
+  end
+  devise_for :users
+   get 'my_books', to: 'books#my_books'
+end
+
